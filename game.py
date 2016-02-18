f314ced40394b5b250400ca0c289b6ea424d2188
Ram Kumar <ramkumar.reddy@students.iiit.ac.in>
2016-02-19 01:31:13 +0530

    Fixed invalid move bug

--- game.py
+++ game.py
@@ -44,18 +44,31 @@ class Player36:
             pos=(4,(2,2))
             return pos
         if(flag=='x' and depth!=0):
-        	board[enemyPos[0]][enemyPos[1]]='x'
+          board[enemyPos[0]][enemyPos[1]]='x'
         if(flag=='o' and depth!=0):
-        	board[enemyPos[0]][enemyPos[1]]='o'
+          board[enemyPos[0]][enemyPos[1]]='o'
         ourBlocks=self.goTo[(enemyPos[0]%3,enemyPos[1]%3)]
-        for iters in len(ourBlocks):
-            if(block[ourBlocks[iters][0]*3+ourBlocks[iters][1]]!='-'):
-                ourBlocks.remove(iters)
+        # print ourBlocks,'--------------'
+        templist=[]
+        for iters in range(len(ourBlocks)):
+            if(block[ourBlocks[iters][0]*3+ourBlocks[iters][1]]=='-'):
+                # print ourBlocks[iters]
+                templist.append(ourBlocks[iters])
+                # ourBlocks.remove(ourBlocks[iters])
+                # print ourBlocks
+        ourBlocks = templist
+        if(len(ourBlocks)==0):
+            templist=[]
+            for aa in range(9):
+                if(block[aa]=='-'):
+                    templist.append((aa/3,aa%3))
+        ourBlocks = templist
+        # print ourBlocks
         if depth==2:
             p=random.randint(-10,10)
             return (p,0)
         else:
-            print ourBlocks,"!!!!!!!!!!!!!!!!!!!"
+            # print ourBlocks,"!!!!!!!!!!!!!!!!!!!"
             for i in range(len(ourBlocks)):
                 cells=ourBlocks[i]
                 base_tuple=self.base[cells]
@@ -67,6 +80,7 @@ class Player36:
                                 for m in range(9):
                                     temp[l][m]=board[l][m]
                             rtuple=self.makeMove(temp,block,(j+base_tuple[0],k+base_tuple[1]),depth+1,flag)
+                            # print rtuple
                             utility=rtuple[0]
                             miniMaxDict[utility]=(j+base_tuple[0],k+base_tuple[1])
         if depth%2==1 and len(miniMaxDict)!=0:
@@ -75,10 +89,10 @@ class Player36:
             return sorted(miniMaxDict.items())[len(miniMaxDict)-1]
 
     def move(self,board,block,enemyPos,flag):
-    	final=self.makeMove(board,block,enemyPos,0,flag)
+      final=self.makeMove(board,block,enemyPos,0,flag)
         # print final
         print '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%',flag,final[1]
-    	return final[1]
+      return final[1]
 
 # flag=sys.argv[1]
 # flag='x'
